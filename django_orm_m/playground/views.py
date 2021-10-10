6a5b14439b430e05ce4e305f9257c62b92ced944
fahim2077 <fahimalsafi@gmail.com>
2021-10-09 23:37:38 -0400

    dajngo-orm done

--- django_orm_m/playground/views.py
+++ django_orm_m/playground/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem, Customer
 
 
 def say_hello(request):
-    query_set = Product.objects.order_by('-unit_price')
+    '''query_set = Product.objects.filter(
+        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')'''
+
+    query_set = Customer.objects.all()[0:5]
 
     return render(request, 'hello.html', {'name': 'Fahim', 'products': list(query_set)})
