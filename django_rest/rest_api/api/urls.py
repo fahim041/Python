007f8ead021ba9525e15aea77274bb47293ff130
Fahim090 <fahimalsafi@gmail.com>
2021-12-23 17:37:08 -0500

    clean_up

--- django_rest/rest_api/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import article_details, article_list, release_list, release_details
-
-urlpatterns = [
-    path('article/', article_list),
-    path('article/<int:pk>', article_details),
-    path('release/', release_list),
-    path('release/<str:pk>', release_details)
-]
--- p_m/list.py
+++
@@ -1,8 +0,0 @@
-items = [
-    ('product1', 10),
-    ('product2', 7),
-    ('product3', 15)
-]
-
-items.sort(key=lambda item: item[1])
-print(items)
